75663c1d1747001fc8e5bb6ef5e3ce9645c765d1
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-11-26 14:47:11 -0800

    Print average time taken to publish @types packages
    1. Grab the last 30 Definitely Typed PRs
    2. Compare when they were merged to when they were published
    3. Print the number of seconds
    4. Print the average number of seconds
    
    Average doesn't give a complete picture since we care about percentiles,
    but it's good enough for a watchdog. 90th percentile might be good in
    the future if we end up with multiple alert levels.

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-console.log("Too slow");
+const sh = require('shelljs')
+const fs = require('fs')
+const Gh = require('@octokit/rest')
+var gh = new Gh()
+
+async function main() {
+    gh.authenticate({
+        type: "token",
+        token: fs.readFileSync('/home/nathansa/api.token', { encoding: 'utf-8' })
+    })
+    const rows = await recentPrs()
+    recentPackages(rows)
+    // fs.writeFileSync('/home/nathansa/types-publisher-watchdog/latency.json', JSON.stringify(rows))
+}
+
+/**
+ * 1. Only match paths that begin with types/
+ * 2. Only match paths that end with a top-level filename
+ * 3. Capture the name of the package and the name of the file.
+ * 3b. (We don't use the name of the file right now.)
+ */
+const THIS_IS_FINE = /^types\/([^\/]+?)\/([^\/]+)$/
+
+/** returns {Promise<Map<string, Date>>} */
+async function recentPrs() {
+    const search = await gh.search.issues({
+        q: "is:pr is:closed repo:DefinitelyTyped/DefinitelyTyped",
+        sort: "updated",
+        order: "desc",
+        per_page: 30,
+        page: 1
+    })
+    /** @type {Map<string, Date>} */
+    const prs = new Map()
+    for (const it of search.data.items) {
+        const mergeDate = (await gh.pulls.get({
+            owner: "DefinitelyTyped",
+            repo: "DefinitelyTyped",
+            number: it.number
+        })).data.merged_at
+        if (mergeDate == null)
+            continue
+        const fileEntries = (await gh.pulls.listFiles({
+            owner: "DefinitelyTyped",
+            repo: "DefinitelyTyped",
+            number: it.number,
+            per_page: 100,
+        })).data
+        /** @type {Set<string>} */
+        const mini = new Set()
+        for (const fileChange of fileEntries) {
+            const m = fileChange.filename.match(THIS_IS_FINE)
+            if (m == null)
+                continue
+            mini.add(m[1])
+        }
+        for (const name of mini) {
+            const date = new Date(mergeDate)
+            const prevDate = prs.get(name)
+            if (!prevDate || date > prevDate) {
+                prs.set(name, date)
+            }
+        }
+    }
+    return prs
+}
+/** @param {Map<string, Date>} prs */
+function recentPackages(prs) {
+    let sum = 0
+    let count = 0
+    for (const [name, mergeDate] of prs) {
+        const publishDate = new Date(sh.exec(`npm info @types/${name} time.modified`, { silent : true }).stdout.trim())
+        if (mergeDate > publishDate) {
+            console.log(name + ': not published yet; latency so far: ' + (Date.now() - mergeDate.valueOf()))
+            sum += Date.now() - mergeDate.valueOf()
+        }
+        else {
+            console.log(name + ': ' + ((publishDate.valueOf() - mergeDate.valueOf()) / 1000))
+            sum += publishDate.valueOf() - mergeDate.valueOf()
+        }
+        count++
+    }
+    console.log('Average: ' + (sum / count / 1000))
+}
+main()
